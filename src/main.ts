840c75c4e87a375b766e5a83aad14a855c4430a1
renan5g <renansilva.dev5g@gmail.com>
2022-08-11 18:48:46 -0300

    chore: add deps and export index

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação do sistema QrCard')
+    .setDescription('Api validador do sistema QrCard')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/shared/domain/value-objects/index.ts
+++ src/shared/domain/value-objects/index.ts
@@ -1,3 +1,5 @@
+export * from './cnpj.value-object';
+export * from './cpf.value-object';
 export * from './email.value-object';
 export * from './name.value-object';
 export * from './password.value-object';
